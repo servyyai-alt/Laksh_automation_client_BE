42c7fc4279536953168a7228665853abeca1e4e6
LeastActionCompany <leastactioncompany@gmail.com>
2026-07-10 13:34:50 +0530

    api limit increased

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(compression());
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
+  max: 10000,
   message: { success: false, message: 'Too many requests, please try again later.' }
 });
 app.use('/api/', limiter);
